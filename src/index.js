a14c63cab4650f57ec1213e6a7bd78af9ef6bed2
umerzzz <m.umerr005@gmail.com>
2024-09-12 14:22:21 +0500

    Setup

--- src/index.js
+++ src/index.js
@@ -4,4 +4,12 @@ import connectDB from "./db/index.js";
 dotenv.config({
   path: "/env",
 });
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 7000, () => {
+      console.log(`Server Up And Running At Port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(`Mongo DB Failed To Establish A Connection : ${err}`);
+  });
